b7fae8f8ee2cfd9f7e64b84feb36c04dd4a6ce07
alextern0 <86599134+alextern0@users.noreply.github.com>
2021-11-13 16:32:53 +0300

    Add files via upload

---
+++ src/components/MessageInput.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { Paper, IconButton, InputBase } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles"
+import { Send } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingLeft: 8,
+    },
+    input: {
+        flexGrow: 1,
+    }
+}))
+
+export const MessageInput = (props) => {
+    const classes = useStyles();
+    const [value, setValue] = useState("");
+
+
+
+    const resetForm = () => {
+        setValue("");
+    };
+
+    const onSubmitMessage = (event) => {
+        event.preventDefault();
+        props.onSend(value);
+        resetForm();
+    };
+
+    const onChangeMessageInput = (event) => {
+        setValue(event.target.value);
+    };
+
+    
+
+    return (
+        <Paper
+            className={classes.paper}
+            component="form"
+            onSubmit={onSubmitMessage}
+            >
+            <InputBase
+                className={classes.input}
+                onChange={onChangeMessageInput}
+                placeholder={props.placeholder}
+                value={value}
+                type="text"
+            />
+            <IconButton>
+                <Send />
+            </IconButton>
+        </Paper>
+    );
+};
+
+MessageInput.propTypes = {
+    onSend: PropTypes.func,
+    placeholder: PropTypes.string
+};
+
+MessageInput.defaultProps = {
+    placeholder: "type message"
+};
